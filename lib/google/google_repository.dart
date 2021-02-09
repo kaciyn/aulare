e2fc7344e439ee13a8ec8b7f4bb251687e3e786c
sectumsemper <kaciy11@yahoo.co.uk>
2021-02-09 15:03:09 +0100

    Deleting google stuff because guess what I am never going to use it in this project

--- lib/google/google_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:aulare/google/google_provider.dart';
-import 'package:firebase_auth/firebase_auth.dart' as firebase;
-
-class GoogleRepository {
-  GoogleProvider googleProvider = GoogleProvider();
-
-  Future<firebase.User> signInWithGoogle() => googleProvider.signInWithGoogle();
-}
