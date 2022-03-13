3bc82243ca6fc96d74578343a270c85239b3520e
kaciyn <kaci.yan@outlook.com>
2022-03-13 22:16:17 +0000

    Mock account page

---
+++ lib/views/account/account_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../../components/menu_drawer.dart';
+import '../../config/defaultTheme.dart';
+import '../../config/transitions.dart';
+import '../contacts/contacts_page.dart';
+
+class AccountPage extends StatelessWidget {
+  const AccountPage({required Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Scaffold(
+            backgroundColor: darkTheme.scaffoldBackgroundColor,
+            endDrawer: MenuDrawer(context: context),
+            body: const FlexibleSpaceBar(
+              centerTitle: true,
+              title: Text(
+                'ACCOUNT',
+              ),
+            )));
+  }
+}
